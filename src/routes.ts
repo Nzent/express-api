c7b60579e27c6337210e7c94f8ed1b24524a8582
NisalK <nirvanzentinal@gmail.com>
2022-10-18 16:38:57 +0530

    validate schema done

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
 
 export default function (app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
   // Register the user
-
+app.post("/api/users",validateRequest(createUserSchema),createUserHandler)
   // Login the user
 
   // Get the user's session
